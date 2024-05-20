9f0e79aeec7321a86a8806fe2f8f539fdd27474a
NoreenBarrera <barreranoreen22@gmail.com>
2024-05-20 11:50:08 +0800

    Update and rename updatedNavBar.js to ignore this.js

--- updatedNavBar.js
+++
@@ -1,4 +0,0 @@
-//JavaScript for NavBar
-type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
-nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
-//JavaScript for NavBar ends here
