f6f7b62e146ce0465919a956789767b6cc118e03
Fujijared2810 <scorpeejared2@gmail.com>
2024-05-20 09:39:59 +0800

    -Added Hero -Added Learn More -More mobile responsive except hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-      colors: {
-        darkblue: '#00008B',
-        verydarkblue: '#00004B',
-        black: colors.black,
-        white: colors.white,
-        gray: colors.gray,
+    colors: {
+      darkblue: '#00008B',
+      verydarkblue: '#00004B',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif'],
       },
-    extend: {},
+    },
   },
   plugins: [],
-}
+};
